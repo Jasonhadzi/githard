c229da7b1fe6a943bf9a3e2e1b0f15a52cf30bce
Iason Hadzikostas <jasonhadzi@gmail.com>
2025-08-06 21:01:32 -0500

    adds protected route so that only authenticated users can access the routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,28 @@ import LoginPage from './LoginPage';
 import ProjectManager from './projectManager';
 import ProjectDashboard from './projectDashboard';
 import HomePage from './HomePage';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/manager/:userId" element={<ProjectManager />} />
-        <Route path="/dashboard/:userId" element={<ProjectDashboard />} />
-        <Route path="/hw/:userId/:projectId" element={<HomePage/>} />
+        <Route path="/manager/:userId" element={
+          <ProtectedRoute>
+            <ProjectManager />
+          </ProtectedRoute>
+        } />
+        <Route path="/dashboard/:userId" element={
+          <ProtectedRoute>
+            <ProjectDashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/hw/:userId/:projectId" element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        } />
       </Routes>
     </Router>
   );
